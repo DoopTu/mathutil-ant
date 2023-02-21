6a6f38fa1f9e20be432b851f4f88b786fa24cc0e
DoopTu <tuhandsome111@gmail.com>
2023-02-21 08:45:52 +0700

    Update code to make it wrong

--- src/com/be/mathutil/core/MathUtil.java
+++ src/com/be/mathutil/core/MathUtil.java
@@ -14,7 +14,7 @@ public class MathUtil {
         if (n == 0 || n == 1) {
             return 1;
         } else {
-            for (int i = 2; i <= n; i++) {
+            for (int i = 2; i < n; i++) { //make code wrong
                 fac *= i;
             }
         }
